b027f1fe98d7e4da0d92760724a44481a8ef4bc7
VeryCleverName3 <07jamesemr@csdecatur.net>
2018-12-11 19:59:07 -0500

    Added blink, gameover, and attacks

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 function update(){
-  updatePlayerPositions();
-  drawPlayers();
-  blinkManager();
+  if(!gameOver){
+    updatePlayerPositions();
+    drawPlayers();
+    blinkManager();
+    attackManager();
+  } else gameOverScreen();
+}
+
+function gameOverScreen(){
+  ctx.fillStyle = winner;
+  ctx.fillRect(0, 0, s, s);
+  ctx.textAlign = "center";
+  ctx.font = "40px impact";
+  ctx.fillStyle = "black";
+  ctx.fillText("Game Over! The winner is " + winner , s / 2, s / 2);
 }
 
 function blinkManager(){
@@ -12,16 +24,70 @@ function blinkManager(){
     p1.blink += 0.01;
   }
 
+
   //same but with p2
   ctx.fillStyle = "red";
   ctx.fillRect(0.5 * s, s - (s/100), p2.blink * (s / 2), s / 100);
   if(p2.blink < 1){
     p2.blink += 0.01;
   }
+
+  if(p1.blinking){
+    p1.blinking = false;
+    p1.newx = p1.x;
+    p1.newy = p1.y;
+    p1.trailTimer = 45;
+  }
+
+  if(keyDown[70] && p1.blink >= 0.75){
+    p1.blink = 0.25;
+    p1.oldx = p1.x;
+    p1.oldy = p1.y;
+    p1.speed *= 60;
+    p1.blinking = true;
+  }
+
+  if(p1.trailTimer > 0){
+    p1.trailTimer--;
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(p1.oldx * s, p1.oldy * s);
+    ctx.lineTo(p1.newx * s, p1.newy * s);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  if(p2.blinking){
+    p2.blinking = false;
+    p2.newx = p2.x;
+    p2.newy = p2.y;
+    p2.trailTimer = 45;
+  }
+
+  if(keyDown[188] && p2.blink >= 0.75){
+    p2.blink = 0.25;
+    p2.speed *= 60;
+    p2.oldx = p2.x;
+    p2.oldy = p2.y;
+    p2.blinking = true;
+  }
+
+  if(p2.trailTimer > 0){
+    p2.trailTimer--;
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(p2.oldx * s, p2.oldy * s);
+    ctx.lineTo(p2.newx * s, p2.newy * s);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  //Draw blink effect
 }
 
 function drawPlayers(){
   ctx.clearRect(0, 0, s, s);
+  ctx.strokeStyle = "black";
   ctx.strokeRect(0, 0, s, s);
 
   //Draw blue square
@@ -46,6 +112,9 @@ function updatePlayerPositions(){
   if(keyDown[68]){
     p1.x += p1.speed;
   }
+  if(p1.speed > 0.005){
+    p1.speed = 0.005;
+  }
   if(keyDown[38]){
     p2.y -= p2.speed;
   }
@@ -57,6 +126,9 @@ function updatePlayerPositions(){
   }
   if(keyDown[39]){
     p2.x += p2.speed;
+  }
+  if(p2.speed > 0.005){
+    p2.speed = 0.005;
   }
   if(p1.x < 0){
     p1.x = 0;
@@ -84,11 +156,50 @@ function updatePlayerPositions(){
   }
 }
 
+function attackManager(){
+  if(keyDown[71] && p1.blink > 0.2){
+    p1.blink -= 0.03;
+    if(p1.blink <= 0.2){
+      p1.blink = 0;
+    }
+    ctx.beginPath();
+    ctx.fillStyle = "darkblue";
+    ctx.arc(p1.x * s, p1.y * s, s / 20, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+    if(Math.hypot(p1.x - p2.x, p1.y - p2.y)  <= (1/20)){
+      console.log("blue wins");
+      gameOver = true;
+      winner = "blue";
+    }
+  }
+  if(keyDown[190] && p1.blink > 0.2){
+    p2.blink -= 0.03;
+    if(p2.blink <= 0.2){
+      p2.blink = 0;
+    }
+    ctx.beginPath();
+    ctx.fillStyle = "darkred";
+    ctx.arc(p2.x * s, p2.y * s, s / 20, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+    if(Math.hypot(p2.x - p1.x, p2.y - p1.y)  <= (1/20)){
+      console.log("red wins");
+      gameOver = true;
+      winner = "red";
+    }
+  }
+}
+
 var c = document.getElementById("gameCanvas");
 var ctx = c.getContext("2d");
 c.height = window.innerHeight;
 c.width = c.height;
 s = c.width;
+
+var gameOver = false;
+
+var winner = "none";
 
 ctx.lineWidth = 10;
 ctx.strokeRect(0, 0, s, s);
@@ -100,14 +211,26 @@ var p1 = {
   x : 0.25,
   y : 0.5,
   speed : 0.005,
-  blink : 1
+  blink : 1,
+  oldx : -1,
+  oldy : -1,
+  newx : -1,
+  newy : -1,
+  blinking : false,
+  trailTimer : 0
 };
 
 var p2 = {
   x : 0.75,
   y : 0.5,
   speed : 0.005,
-  blink : 1
+  blink : 1,
+  oldx : -1,
+  oldy : -1,
+  newx : -1,
+  newy : -1,
+  blinking : false,
+  trailTimer : 0
 };
 
 onkeydown = function(e){
